45ecdf468c2bb7e3928132b66f57c3f3e31de44c
hetbhalodiya09 <110375429+hetbhalodiya09@users.noreply.github.com>
2022-08-27 16:34:36 +0530

    Add files via upload

---
+++ module1/task/8-8/Age.java
@@ -0,0 +1,29 @@
+package my;
+
+public class Age 
+{
+	static void validate(int age)
+	{
+		if(age>18)
+		{
+			System.out.println("you are eligible");
+		}
+		else
+		{
+			try
+			{
+				throw new MyException();
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	public static void main(String[] args) 
+	{
+		validate(14);
+	}
+}
